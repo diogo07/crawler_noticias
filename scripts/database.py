922d2e522d659c7abd34aeb8cac10fdbf8b02aab
diogo07 <diogosousa36@gmail.com>
2019-09-13 09:27:19 -0300

    Limpeza de código

--- scripts/database.py
+++ scripts/database.py
@@ -9,7 +9,7 @@ def insert_noticias(noticias):
                                           port="5432",
                                           database="db_noticias")
             cursor = connection.cursor()
-            postgres_insert_query = """ INSERT INTO noticias (titulo, descricao, conteudo, data, url_image) VALUES (%s,%s,%s,%s,%s)"""
+            postgres_insert_query = """ INSERT INTO noticias (titulo, descricao, conteudo, created_at, path_image) VALUES (%s,%s,%s,%s,%s)"""
             record_to_insert = (str(noticia.titulo), str(noticia.descricao), str(noticia.conteudo), str(noticia.data), str(noticia.url_image))
             cursor.execute(postgres_insert_query, record_to_insert)
             connection.commit()
